4a51158abd384572705227fee43965d545fdbe1d
nahom1231 <nahom.getnet123@gmail.com>
2026-01-30 15:42:55 +0300

    Even tho the templates are not working

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'pages',    
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "accounts.CustomUser"
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
